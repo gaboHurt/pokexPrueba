4e5447e53344816b48bcf71a4ed6549a8ee7c64a
gaboHurt <80229071+gaboHurt@users.noreply.github.com>
2021-03-12 12:19:35 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -5,17 +5,14 @@ export default function formPokemon() {
       e.preventDefault();
       if (e.target['input-pokemon'].value.trim() === '') {
         d.querySelector('.modal').classList.add('active');
+        d.querySelector('.container').classList.add('modal-filter');
         return;
       }
       console.log('click');
       if (location.pathname === '/pokedex_Gabo/index.html') {
-        location.href = `./v/pokemon.html?idName=${e.target[
-          'input-pokemon'
-        ].value.toLowerCase()}`;
+        location.href = `./v/pokemon.html?idName=${e.target['input-pokemon'].value.toLowerCase()}`;
       } else {
-        location.href = `./pokemon.html?idName=${e.target[
-          'input-pokemon'
-        ].value.toLowerCase()}`;
+        location.href = `./pokemon.html?idName=${e.target['input-pokemon'].value.toLowerCase()}`;
       }
       e.target['input-pokemon'].value = '';
     }
